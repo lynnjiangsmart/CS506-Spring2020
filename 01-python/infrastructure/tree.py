54a6ac44637ad9dbf4c3f126715fe58bad4ddb62
Ziyi Jiang <lynnj@csa2.bu.edu>
2020-01-27 17:45:46 -0500

    Add trees

--- 01-python/infrastructure/tree.py
+++ 01-python/infrastructure/tree.py
@@ -1,3 +1,35 @@
-def draw_tree():
-    print("tree not found")
-    return
+def draw_tree(shape='nora'):
+	VALID = ['nora','lynn']
+	if shape not in VALID:
+		raise ValueError('shape is undefiend')
+	
+	if shape == 'nora':
+    		print(" "," ","n"," "," ")
+    		print()
+    		print(" ","n"," ", "n "," ")
+    		print ()
+    		print("n"," "," "," ","n")
+    		print()
+    		print("  ","||","  ")
+    		return    
+	
+
+		 
+
+	else:
+	   print(" "," ","l"," "," ")
+    	   print()
+    	   print(" ","l"," ", "l "," ")
+    	   print ()
+    	   print("l"," "," "," ","l")
+    	   print()
+    	   print("  ","||","  ")
+	
+	return
+
+
+
+
+
+
+
